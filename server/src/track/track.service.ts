668bfabe5f81b0a6f45dda81281be47febd28fc3
Chupriarti <wallx13@gmail.com>
2022-02-10 22:05:37 +0300

    add track comment api

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model, ObjectId } from "mongoose";
+import { CreateCommentDto } from "./dto/create-comment.dto";
 import { CreateTrackDto } from "./dto/create-track.dto";
 import { Comment, CommentDocument } from "./schemas/comment.schema";
 import { Track, TrackDocument } from "./schemas/track.schema";
@@ -28,6 +29,14 @@ export class TrackService {
   async delete(id: ObjectId): Promise<ObjectId>{
     const track = await this.trackModel.findByIdAndDelete(id)
     return track._id
+  }
+
+  async addComment(dto: CreateCommentDto): Promise<Comment>{
+    const track = await this.trackModel.findById(dto.trackId)
+    const comment = await this.commentModel.create({...dto})
+    track.comments.push(comment._id)
+    await track.save()
+    return comment
   }
 
 }
