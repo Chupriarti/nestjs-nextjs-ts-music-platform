89088f9c275e5797a1e02c9fccd4110bd1d0b3c2
Chupriarti <wallx13@gmail.com>
2022-02-10 21:47:33 +0300

    add create track api

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateTrackDto } from "./dto/create-track.dto";
+import { TrackService } from "./track.service";
 
 @Controller('/tracks')
 export class TrackController {
-  create(){
+  constructor(private trackService: TrackService){}
 
+  @Post()
+  create(@Body() dto: CreateTrackDto){
+    return this.trackService.create(dto)
   }
 
   getAll(){
--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { CreateTrackDto } from "./dto/create-track.dto";
 import { Comment, CommentDocument } from "./schemas/comment.schema";
 import { Track, TrackDocument } from "./schemas/track.schema";
 
@@ -9,8 +10,9 @@ export class TrackService {
   constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
               @InjectModel(Comment.name) private commentModel: Model<CommentDocument>) {}
 
-  async create(){
-
+  async create(dto: CreateTrackDto): Promise<Track>{
+    const track = await this.trackModel.create({...dto, listens: 0})
+    return track
   }
 
   async getAll(){
