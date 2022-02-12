c490dfa2fb3b5fb78d886218a93b9af71544f81b
Chupriarti <wallx13@gmail.com>
2022-02-12 20:47:55 +0300

    fix

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -21,7 +21,7 @@ export class TrackService {
   }
 
   async getAll(count = 10, offset = 0): Promise<Track[]>{
-    const tracks = await this.trackModel.find().skip(offset).limit(count)
+    const tracks = await this.trackModel.find().skip(Number(offset)).limit(Number(count))
     return tracks
   }
 
